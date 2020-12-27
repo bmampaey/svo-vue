1577a29ff98fc82e40d2544af8f3d41bdb6148e9
Benjamin Mampaey <benjamin.mampaey@oma.be>
2020-12-27 17:17:44 +0100

    Used b-table for Metadata details

--- src/components/globals/MetadataDetail.vue
+++ src/components/globals/MetadataDetail.vue
@@ -1,20 +1,19 @@
 <template>
 	<b-modal ref="metadataDetail" :title="dataset.name" hide-footer>
 		<b-img :src="imageURL" fluid-grow alt="Data thumbnail"></b-img>
-		<b-table-simple small>
-			<thead>
-				<tr>
-					<th>
-						<b-form-input v-model="cardFilter" type="text" placeholder="Filter on FITS keywords"></b-form-input>
-					</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr v-for="card in filteredCards" :key="card.id">
-					<td>{{ card.text }}</td>
-				</tr>
-			</tbody>
-		</b-table-simple>
+
+		<b-table :items="cards" :fields="fields" :filter="searchFilter" :filter-included-fields="filterIncludedFields" primary-key="id" sticky-header show-empty small>
+			<template #head(text)="data">
+				<b-form-group>
+					<b-input-group>
+						<b-form-input v-model="searchFilter" type="search" placeholder="Filter metadata" debounce="500"></b-form-input>
+						<b-input-group-append>
+							<b-button :disabled="!searchFilter" @click="clearSearchFilter">Clear</b-button>
+						</b-input-group-append>
+					</b-input-group>
+				</b-form-group>
+			</template>
+		</b-table>
 	</b-modal>
 </template>
 
@@ -27,32 +26,34 @@ export default {
 	},
 	data: function() {
 		return {
-			cardFilter: ""
+			searchFilter: "",
+			fields: ["text"],
+			filterIncludedFields: ["text"]
 		};
 	},
 	methods: {
 		show: function() {
 			this.$refs.metadataDetail.show();
+		},
+		clearSearchFilter: function() {
+			this.searchFilter = "";
 		}
 	},
 	computed: {
 		imageURL: function() {
-			return this.metadata.data_location.thumbnail_url ? this.metadata.data_location.thumbnail_url : "./no_preview_available.jpg";
+			return this.metadata.data_location.thumbnail_url ? this.metadata.data_location.thumbnail_url : require("@/assets/no_preview_available.jpg");
 		},
 		cards: function() {
 			let cards = [];
-			if (this.metadata.fits_header) {
-				cards = this.metadata.fits_header.match(/[^]{1,80}/g).map((text, id) => ({ id, text }));
+			let fits_header = this.metadata.fits_header ? this.metadata.fits_header.trimEnd() : null;
+			if (fits_header) {
+				cards = fits_header.match(/[^]{1,80}/g).map((text, id) => ({ id, text }));
 			} else {
 				cards = Object.entries(this.metadata)
 					.filter(([, value]) => !(value instanceof Object))
 					.map(([key, value], id) => ({ id, text: `${key} = ${value}` }));
 			}
 			return cards;
-		},
-		filteredCards: function() {
-			let regex = this.cardFilter ? new RegExp(this.cardFilter, "i") : /\S/;
-			return this.cards.filter(card => regex.test(card.text));
 		}
 	}
 };
